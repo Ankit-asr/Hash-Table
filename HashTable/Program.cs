3e585a7c5627e9630489352f860455673f49337b
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-23 17:56:04 +0530

    [Ankit] Add:Frequency of words in a sentence

---
+++ HashTable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "To be or not to be";
+            ShowFrequency(sentence);
+        }
+        public static void ShowFrequency(string sentence)
+        {
+            bool checkFirst = true;
+            string[] wordList = sentence.Split(" ");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(wordList.Length);
+            foreach (string word in wordList)
+            {
+                if (checkFirst)
+                {
+                    hash.Add(word, "1");
+                    checkFirst = false;
+                }
+                else
+                {
+                    if (hash.Get(word) != null)
+                    {
+                        int count = Convert.ToInt16(hash.Get(word));
+                        count++;
+                        hash.Remove(word);
+                        hash.Add(word, count.ToString());
+                    }
+                    else
+                    {
+                        hash.Add(word, "1");
+                    }
+                }
+            }
+            foreach (string key in wordList)
+            {
+                Console.WriteLine(key + " Value is :" + hash.Get(key));
+            }
+        }
+    }
+}
